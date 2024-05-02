55426706fa42631e4d0e6cb4737440aab592bc92
Aaron Brady <aaron@insom.me.uk>
2024-05-02 19:10:04 -0400

    force-merge from alterate-history

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,104 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
-use defmt_rtt as _;
-use embassy_executor::Spawner;
-use embassy_rp::i2c::{self, Config};
-use embassy_time::Timer;
-use embedded_hal_1::i2c::I2c;
-use panic_probe as _;
+use cortex_m::delay::Delay;
+use panic_halt as _;
 
-#[allow(dead_code)]
-mod lcd1602 {
-    pub const ADDR: u8 = 0x27;
-    pub const LCD_ENABLE: u8 = 0x04;
-    pub const LCD_BACKLIGHT: u8 = 0x08;
-    pub const LCD_COMMAND: u8 = 0x00;
-    pub const LCD_CHARACTER: u8 = 0x01;
-    pub const LCD_CLEARDISPLAY: u8 = 0x01;
-    pub const LCD_RETURNHOME: u8 = 0x02;
-    pub const LCD_ENTRYMODESET: u8 = 0x04;
-    pub const LCD_DISPLAYCONTROL: u8 = 0x08;
-    pub const LCD_CURSORSHIFT: u8 = 0x10;
-    pub const LCD_FUNCTIONSET: u8 = 0x20;
-    pub const LCD_SETCGRAMADDR: u8 = 0x40;
-    pub const LCD_SETDDRAMADDR: u8 = 0x80;
-    pub const LCD_ENTRYSHIFTINCREMENT: u8 = 0x01;
-    pub const LCD_BLINKON: u8 = 0x01;
-    pub const LCD_CURSORON: u8 = 0x02;
-    pub const LCD_DISPLAYON: u8 = 0x04;
-    pub const LCD_MOVERIGHT: u8 = 0x04;
-    pub const LCD_DISPLAYMOVE: u8 = 0x08;
-    pub const LCD_5X10DOTS: u8 = 0x04;
-    pub const LCD_2LINE: u8 = 0x08;
-    pub const LCD_8BITMODE: u8 = 0x10;
-    pub const LCD_ENTRYLEFT: u8 = 0x02;
+use embedded_hal::digital::OutputPin;
+use hal::pac;
+use rp2040_hal as hal;
+use rp2040_hal::clocks::Clock;
+
+use rtt_target::*;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+pub struct Beeper<P> {
+    delay: Delay,
+    pin: P,
 }
 
-pub struct Lcd1602<I> {
-    i2c: I,
-}
-
-impl<I: I2c> Lcd1602<I> {
-    async fn init(mut self) {
-        use lcd1602::*;
-        self.send_byte(3, LCD_COMMAND).await;
-        self.send_byte(3, LCD_COMMAND).await;
-        self.send_byte(3, LCD_COMMAND).await;
-        self.send_byte(2, LCD_COMMAND).await;
-        self.send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND).await;
-        self.send_byte(LCD_FUNCTIONSET | LCD_2LINE, LCD_COMMAND).await;
-        self.send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND).await;
-        self.send_byte(LCD_CLEARDISPLAY, LCD_COMMAND).await;
-        self.send_byte('B' as u8, LCD_CHARACTER).await;
+impl<P: OutputPin> Beeper<P> {
+    pub fn start(&mut self) {
+        self.pin.set_high().unwrap();
+        self.delay.delay_us(1500);
+        self.pin.set_low().unwrap();
+        self.delay.delay_us(741);
     }
-
-    async fn send_byte(&mut self, value: u8, mode: u8) {
-        use lcd1602::*;
-        let high: u8 = mode | (value & 0xf0u8) | LCD_BACKLIGHT;
-        let low: u8 = mode | ((value << 4) & 0xf0u8) | LCD_BACKLIGHT;
-
-        self.i2c.write(0x27u8, &[high]).unwrap();
-        self.toggle_enable(high).await;
-        self.i2c.write(0x27u8, &[low]).unwrap();
-        self.toggle_enable(low).await;
-    }
-
-    async fn toggle_enable(&mut self, value: u8) {
-        use lcd1602::*;
-        let i2c = &mut self.i2c;
-        Timer::after_micros(600).await;
-        i2c.write(0x27u8, &[value | LCD_ENABLE]).unwrap();
-        Timer::after_micros(600).await;
-        i2c.write(0x27u8, &[value & !LCD_ENABLE]).unwrap();
-        Timer::after_micros(600).await;
+    pub fn send(&mut self, output: &str) {
+        for character in output.chars() {
+            if character == '1' {
+                self.pin.set_high().unwrap();
+                self.delay.delay_us(741);
+                self.pin.set_low().unwrap();
+                self.delay.delay_us(247);
+            } else if character == '0' {
+                self.pin.set_high().unwrap();
+                self.delay.delay_us(247);
+                self.pin.set_low().unwrap();
+                self.delay.delay_us(741);
+            }
+        }
+        self.delay.delay_us(7000);
     }
 }
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
 
-    let sda = p.PIN_4;
-    let scl = p.PIN_5;
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    let i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, Config::default());
+    let clocks = hal::clocks::init_clocks_and_plls(
+        12_000_000u32,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
-    info!("lo");
+    let sio = hal::Sio::new(pac.SIO);
 
-    let l = Lcd1602::<_> { i2c };
-    l.init().await;
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    rtt_init_print!();
+
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+
+    let radio_pin = pins.gpio15.into_push_pull_output();
+    let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut b = Beeper { delay: delay, pin: radio_pin };
 
     loop {
-        info!("1s");
-        Timer::after_secs(1).await;
+        led_pin.set_high().unwrap();
+        b.delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        b.delay.delay_ms(500);
+        rprintln!("Loop!");
+
+        for _ in 0..5 {
+            b.start();
+            // high power v
+            // b.send("1 000 0010 00110101001000100 1100100 1011 111 00");
+            // low power v
+            // b.send("1 000 0010 00110101001000100 0000100 1011 111 00");
+            // low power s
+            b.send("1 000 0001 00110101001000100 0000001 0111 111 00");
+        }
+
+        rprintln!("Sent!");
+        b.delay.delay_ms(2000);
     }
 }
