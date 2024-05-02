0906db4b4ee5f96c81fdc9ac4982552b0595c122
Aaron Brady <aaron@insom.me.uk>
2024-05-02 17:12:10 -0400

    Pain-staking development

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::delay::Delay;
 use panic_halt as _;
 
 use embedded_hal::digital::OutputPin;
@@ -13,6 +14,36 @@ use rtt_target::*;
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+pub struct Beeper<P> {
+    delay: Delay,
+    pin: P,
+}
+
+impl<P: OutputPin> Beeper<P> {
+    pub fn start(&mut self) {
+        self.pin.set_high().unwrap();
+        self.delay.delay_us(1500);
+        self.pin.set_low().unwrap();
+        self.delay.delay_us(741);
+    }
+    pub fn send(&mut self, output: &str) {
+        for character in output.chars() {
+            if character == '1' {
+                self.pin.set_high().unwrap();
+                self.delay.delay_us(741);
+                self.pin.set_low().unwrap();
+                self.delay.delay_us(247);
+            } else if character == '0' {
+                self.pin.set_high().unwrap();
+                self.delay.delay_us(247);
+                self.pin.set_low().unwrap();
+                self.delay.delay_us(741);
+            }
+        }
+        self.delay.delay_us(7000);
+    }
+}
 
 #[rp2040_hal::entry]
 fn main() -> ! {
@@ -33,8 +64,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let sio = hal::Sio::new(pac.SIO);
 
     let pins = hal::gpio::Pins::new(
@@ -47,13 +76,29 @@ fn main() -> ! {
     rtt_init_print!();
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
-    let mut radio_pin = pins.gpio15.into_push_pull_output();
+
+    let radio_pin = pins.gpio15.into_push_pull_output();
+    let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut b = Beeper { delay: delay, pin: radio_pin };
 
     loop {
         led_pin.set_high().unwrap();
-        delay.delay_ms(500);
+        b.delay.delay_ms(500);
         led_pin.set_low().unwrap();
-        delay.delay_ms(500);
-        rprintln!("Loop!")
+        b.delay.delay_ms(500);
+        rprintln!("Loop!");
+
+        for _ in 0..5 {
+            b.start();
+            // high power v
+            // b.send("1 000 0010 00110101001000100 1100100 1011 111 00");
+            // low power v
+            // b.send("1 000 0010 00110101001000100 0000100 1011 111 00");
+            // low power s
+            b.send("1 000 0001 00110101001000100 0000001 0111 111 00");
+        }
+
+        rprintln!("Sent!");
+        b.delay.delay_ms(2000);
     }
 }
