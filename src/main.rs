6242f0fbbc738792f0bb8624f826cc810ecb4817
Aaron Brady <aaron@insom.me.uk>
2024-05-06 15:03:05 -0400

    Add randomness

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use trinket_m0::entry;
 use trinket_m0::hal::{self as hal, hal::digital::v2::OutputPin, delay::Delay};
 use trinket_m0::prelude::_embedded_hal_blocking_delay_DelayMs;
 use trinket_m0::prelude::_embedded_hal_blocking_delay_DelayUs;
+use rand::{SeedableRng, Rng};
 
 use rtt_target::*;
 
@@ -55,24 +56,42 @@ fn main() -> ! {
     let mut led_pin = pins.pa10.into_push_pull_output();
 
     let radio_pin = pins.pa02.into_push_pull_output();
-    //let delay = cortex_m::delay::Delay::new(core.SYST, clock.system_clock.freq().to_Hz());
     let delay = Delay::new(core.SYST, &mut clock);
     let mut b = Beeper { delay: delay, pin: radio_pin };
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(0xcafef00ddeadbeef);
 
     loop {
-        led_pin.set_high().unwrap();
-        b.delay.delay_ms(500u32);
-        led_pin.set_low().unwrap();
-        b.delay.delay_ms(500u32);
-        rprintln!("Loop!");
+        let wait = rng.gen_range(10..30);
+        rprintln!("Looping for {}.", wait);
+        for i in 0..wait {
+            led_pin.set_high().unwrap();
+            b.delay.delay_ms(500u32);
+            led_pin.set_low().unwrap();
+            b.delay.delay_ms(500u32);
+            rprintln!("Loop: {}", i);
+        }
 
         for _ in 0..5 {
             b.start();
-            // high power v
-            // b.send("1 000 0010 00110101001000100 1100100 1011 111 00");
             // low power v
-            // b.send("1 000 0010 00110101001000100 0000100 1011 111 00");
+            rprintln!("Sending v");
+            b.send("1 000 0010 00110101001000100 0000100 1011 111 00");
+        }
+
+        let wait = rng.gen_range(2..10);
+        rprintln!("Looping for {}.", wait);
+        for i in 0..wait {
+            led_pin.set_high().unwrap();
+            b.delay.delay_ms(500u32);
+            led_pin.set_low().unwrap();
+            b.delay.delay_ms(500u32);
+            rprintln!("Loop: {}", i);
+        }
+
+        for _ in 0..5 {
+            b.start();
             // low power s
+            rprintln!("Sending s");
             b.send("1 000 0001 00110101001000100 0000001 0111 111 00");
         }
 
